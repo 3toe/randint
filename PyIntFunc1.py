3c67ac209bc81a6a00cfd19b4319258cf48b773c
3toe <AnthonyJS@gmail.com>
2021-08-18 21:37:58 -0700

    added error catcher for min > max

--- PyIntFunc1.py
+++ PyIntFunc1.py
@@ -9,6 +9,8 @@ the goal of this assignment is to write a single function, randInt() that takes 
 import random
 
 def randInt(min=0, max=100):
+   if min > max:
+      min, max = max, min
    num = round(random.random() * (max-min) + min)
    return num
 
@@ -16,3 +18,5 @@ def randInt(min=0, max=100):
 # print(randInt(max=50)) 	         # should print a random integer between 0 to 50
 # print(randInt(min=50)) 	         # should print a random integer between 50 to 100
 # print(randInt(min=50, max=500))   # should print a random integer between 50 and 500
+# print(randInt(100, 0))            # check our error catcher
+# print (randInt(0, -100))          # check our error catcher further
